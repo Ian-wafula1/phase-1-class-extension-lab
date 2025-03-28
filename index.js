6b64d8efbe18e6f83933c79b82ddf95ed2a5c2e4
Ian Wafula <neocorpse@proton.me>
2025-03-28 16:43:46 +0300

    Create Polygon class and relevant children classes

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+class Polygon {
+    constructor(lengths) {
+        this.lengths = lengths
+    }
+
+    get countSides() {
+        return this.lengths.length
+    }
+
+    get perimeter() {
+        return this.lengths.reduce((perimeter, side) => {
+            return perimeter += +side
+        }, 0)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid(){
+        for (let side of this.lengths) {
+            if (this.perimeter - +side < side) return false
+        }
+        return true
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        for (let side of this.lengths) {
+            if (this.perimeter !== +side * 4) return false
+        }
+        return true
+    }
+
+    get area() {
+        return this.lengths[0] ** 2
+    }
+}
